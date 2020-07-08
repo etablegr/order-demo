917f8fd3c60caf1923dc8fd3521ddfb66c99fc54
Dimitrios Desyllas <pc-magas@users.noreply.github.com>
2020-07-08 09:53:27 +0300

    ETABLEWEB-1396 Use Mysql2

--- on_connect/index.js
+++ on_connect/index.js
@@ -1,28 +1,28 @@
-const mysql = require('mysql');
+const mysql = require('mysql2/promise');
 
-const dbconnection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-});
-
-exports.handler = async event => {
+exports.handler = async (event,context,callback) => {
 
     // Api Gateway connection identifier
     let connection_id = event.requestContext.connectionId;
 
-    let sql = "INSERT INTO websocket_connections (connection_id) VALUES (?)";
+    const dbconnection = await mysql.createConnection({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+    });
 
-    dbconnection.connect();
-    try{
-       await dbconnection.query(sql, [connection_id]);
+    let sql = "INSERT INTO websocket_connections (connection_id) VALUES (?)";
+    let response = { statusCode: 200, body: 'Connected.' };
+
+    try {
+       await dbconnection.execute(sql, [connection_id]);
     } catch (err) {
         console.log('Failed to store into database', err.toString());
-        return {statusCode: 500, body: 'Failed To Connect'}
+        response = { statusCode: 500, body: 'Failed To Connect' };
+    } finally {
+        dbconnection.end();
     }
 
-    dbconnection.end();
-
-    return { statusCode: 200, body: 'Connected.' };
+    return response;
 }
--- on_disconnect/index.js
+++ on_disconnect/index.js
@@ -1,26 +1,29 @@
-const mysql = require('mysql');
+const mysql = require('mysql2/promise');
 
-const dbconnection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-});
+
 
 exports.handler = async event => {
     let connection_id = event.requestContext.connectionId;
     let sql = "DELETE FROM websocket_connections WHERE connection_id = ?";
 
-    dbconnection.connect();
+    const dbconnection = await mysql.createConnection({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+    });
+
+    let response = { statusCode: 200, body: 'Connected.' };
 
     try {
-        await dbconnection.query(sql, [connection_id]);
+        await dbconnection.execute(sql, [connection_id]);
     } catch(err) {
         // console.log output is written into cloudwatch logs.
         console.log('Failed to store into database', err.toString());
-        return {statusCode: 500, body: 'Failed To Connect'}
+        response = { statusCode: 500, body: 'Failed To Connect' };
+    } finally {
+        dbconnection.end();
     }
 
-    dbconnection.end();
-    return { statusCode: 200, body: 'Connected.' };
+    return response;
 }
